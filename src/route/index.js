0eff0a90295afb123ef7f443c099ff9e789324f0
SvitlanaNecheta <sn15051973@gmail.com>
2023-11-11 00:51:46 +0200

    HW

--- src/route/index.js
+++ src/route/index.js
@@ -1,13 +1,20 @@
 // Підключаємо роутер до бек-енду
 const express = require('express')
 const router = express.Router()
-
+router.get('/', function (req, res) {
+  res.render('index', {
+    name: 'index',
+    component: [],
+    title: 'Home page',
+    data: {},
+  })
+})
 // Підключіть файли роутів
-const test = require('./test')
+const auth = require('./auth')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
-router.use('/', test)
+router.use('/', auth)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
